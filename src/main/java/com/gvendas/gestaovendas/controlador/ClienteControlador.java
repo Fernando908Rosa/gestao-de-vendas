4f9f2453ea3b99e214a02774fec55f24934fd32f
fernandorosa <fernandodarosa908@gmail.com>
2022-05-13 15:04:28 -0300

    Deletando Cliente no banco de dados

--- src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
+++ src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
@@ -9,11 +9,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gvendas.gestaovendas.dto.cliente.ClienteRequestDTO;
@@ -54,10 +56,19 @@ public class ClienteControlador {
 		return ResponseEntity.status(HttpStatus.CREATED).body(ClienteResponseDTO.converteParaClienteDTO(clienteSalvo));
 
 	}
+
 	@ApiOperation(value = "Atualizar", nickname = "atualizarCliente")
 	@PostMapping("/{codigo")
-	public ResponseEntity<ClienteResponseDTO> atualizar(@PathVariable Long codigo, @Valid @RequestBody ClienteRequestDTO clienteDto) {
+	public ResponseEntity<ClienteResponseDTO> atualizar(@PathVariable Long codigo,
+			@Valid @RequestBody ClienteRequestDTO clienteDto) {
 		Cliente clienteAtualizado = clienteServico.atualizar(codigo, clienteDto.converterParaEntidade(codigo));
 		return ResponseEntity.ok(ClienteResponseDTO.converteParaClienteDTO(clienteAtualizado));
+	}
+
+	@ApiOperation(value = "Deletar", nickname = "deletarCliente")
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		clienteServico.deletar(codigo);
 	}
 }
